721b0485cbfe45de5af602174f9250f6e11fb1c7
Furukawa Kouki <67946339+Lukiw928@users.noreply.github.com>
2021-09-13 11:28:37 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@ const StageApp = Vue.createApp({
       okMessage: '正解！',
       ngMessage: 'そのキーワードは違うようだぞ！？',
       /* 解答 */
-      stage1CorrectAnswer: 'あああ',
+      stage1CorrectAnswer: 'なんばんぼうえき',
       stage201CorrectAnswer: 'いいい',
       stage202CorrectAnswer: 'ううう',
       stage203CorrectAnswer: 'えええ',
